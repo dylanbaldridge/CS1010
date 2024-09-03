bf2808c36615a764e66df76de4b2b8d2c04ffd63
dylanbaldridge <51422522+dylanbaldridge@users.noreply.github.com>
2024-09-02 22:32:42 -0400

    Add files via upload
    week 2 int

---
+++ Lab1.cpp
@@ -0,0 +1,23 @@
+// Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "*\n";
+    std::cout << "**\n";
+    std::cout << "***\n";
+    std::cout << "****\n";
+    std::cout << "*****\n";
+    std::cout << "******\n";
+    std::cout << "*******\n";
+    std::cout << "********\n";
+    std::cout << "*********\n";
+    std::cout << "**********\n";
+};
+
+
+ // My name is Dylan Baldridge so the initals i used was D and B
+// D = 68 and B = 66 so (68 + 66) = 134 / 6 = 23.33333
+// so my remandier ia 3 so my symboly is a right triangle
---
+++ module_2_labs.cpp
@@ -0,0 +1,114 @@
+// module_2_labs.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+    // Lab Problem 1
+    cout << "Lab Problem 1";
+    cout << "\n\n";
+
+    string firstName = "Dylan";
+    string lastName = "Baldridge";
+    string fullName = firstName + lastName ;
+
+    cout << fullName;
+
+
+    cout << "\n\n";
+
+
+
+
+
+    // Lab Problem 2
+    cout << "Lab Problem 2";
+    cout << "\n\n";
+
+
+    int score;
+    score = 21;
+
+    string teamName = "Steelers: ";
+
+        cout << teamName << score ; 
+
+    cout << "\n\n";
+
+
+
+
+    
+    
+    // Lab Problem 3
+
+
+    string myName = "Dylan Baldridge";
+    int age = 32;
+    double myLastMealCost = 23.65;
+    bool superHero = false;
+
+
+
+
+
+    // Lab Problem 4
+
+    cout << "Lab Problem 4";
+    cout << "\n\n";
+
+
+    cout << "Full Name:" << myName << "\n";
+    cout << "My Age:" << age << "\n";
+    cout << "Cost of my last meal:" << myLastMealCost << "\n";
+    cout << "I'm i a superhero?" << superHero << "\n";
+
+    cout << "\n\n";
+
+
+
+
+    // Lab Problem 5
+
+      myName = "Optomus Prime";
+      age = 3589;
+      myLastMealCost = 456.28;
+      superHero = true;
+
+
+
+
+     // Lab Problem 6
+
+
+    cout << "Lab Problem 6";
+    cout << "\n\n";
+
+    cout << "My real name: " << myName << "\n";
+    cout << "My hidden age: " << age << "\n";
+    cout << "Cost of my last fueling: " << myLastMealCost << "\n";
+    cout << "I'm  superhero? " << superHero << "\n";
+
+    cout << "\n\n"; 
+
+    // Lab Problem 7
+
+    cout << "Lab Problem 7";
+    cout << "\n\n";
+
+    cout << sizeof(myName) << " bytes\n";
+    cout << sizeof(age) << " bytes\n";
+    cout << sizeof(myLastMealCost) << " bytes\n";
+    cout << sizeof(superHero) << " bytes\n";
+    
+
+    cout << "\n\n";
+
+
+
+    return 0;
+}
+
